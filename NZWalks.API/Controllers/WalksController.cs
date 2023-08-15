88280097fe9f12baa9c28d532243c3a69c58bcee
Folalu Tomiwa <tomiczilla@gmail.com>
2023-08-15 23:25:33 +0100

    Added pagination to wlks

--- NZWalks.API/Controllers/WalksController.cs
+++ NZWalks.API/Controllers/WalksController.cs
@@ -37,11 +37,14 @@ namespace NZWalks.API.Controllers
         }
 
         //GET Walks
-        // GET: /api/walks?filterOn=Name&filterQuery={query parameter}
+        // GET: /api/walks?filterOn=Name&filterQuery={query parameter}&sortBy={Name}&isAscending=true&pageNumber=1&pageSize=1000
         [HttpGet]
-        public async Task<IActionResult> GetAll([FromQuery] string? filterOn, [FromQuery] string? filterQuery)
+        public async Task<IActionResult> GetAll([FromQuery] string? filterOn, [FromQuery] string? filterQuery, 
+            [FromQuery] string? sortBy, [FromQuery] bool? isAscending, [FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 1000)
+
         {
-            var walksDomainModel = await walkRepository.GetAllAsync(filterOn, filterQuery);
+            var walksDomainModel = await walkRepository.GetAllAsync(filterOn, filterQuery, sortBy, 
+                isAscending ?? true, pageNumber, pageSize);
 
             //Map Domain Model to DTO 
             return Ok(mapper.Map<List<WalkDto>>(walksDomainModel));
--- NZWalks.API/Repositories/SQLWalkRepository.cs
+++ NZWalks.API/Repositories/SQLWalkRepository.cs
@@ -33,7 +33,8 @@ namespace NZWalks.API.Repositories
             return existingWalk;
         }
 
-        public async Task<List<Walk>> GetAllAsync(string? filterOn = null, string? filterQuery = null)
+        public async Task<List<Walk>> GetAllAsync(string? filterOn = null, string? filterQuery = null, 
+            string? sortBy = null, bool isAscending = true, int pageNumber = 1, int pageSize = 1000)
         {
             var walks = dbContext.Walks.Include("Difficulty").Include("Region").AsQueryable();
 
@@ -46,7 +47,25 @@ namespace NZWalks.API.Repositories
                 }
                 
             }
-            return await walks.ToListAsync();
+
+            // Sorting  
+            if (!string.IsNullOrWhiteSpace(sortBy))
+            {
+                if(sortBy.Equals("Name", StringComparison.OrdinalIgnoreCase))
+                {
+                    walks = isAscending ? walks.OrderBy(x => x.Name) : walks.OrderByDescending(x => x.Name);
+                }
+                else if (sortBy.Equals("Length", StringComparison.OrdinalIgnoreCase))
+                {
+                    walks = isAscending ? walks.OrderBy(x => x.LengthInKm) : walks.OrderByDescending(x => x.LengthInKm);
+                }
+            }
+
+            //Pagination
+            var skipResults = (pageNumber - 1) * pageSize;
+            return await walks.Skip(skipResults).Take(pageSize).ToListAsync();
+            //without pagination
+            //return await walks.ToListAsync();
             //return await dbContext.Walks.Include("Difficulty").Include("Region").ToListAsync();
         }
 
